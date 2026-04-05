92ccfc9ab314dd8db90297e6af2fcb803820c45a
Ahmad-khan-1 <qurayshi246@gmail.com>
2026-04-05 13:02:46 +0500

    practice on props

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,29 @@ import { Greeting, UserCheck } from './components/day-1-conditional-rendering/sc
 import { FruitList, ListRendering } from './components/day-2-render-list-in-react/scrip'
 
 function App() {
-  const loggedIN= "true"
+  const loggedIN = "true"
+  const profiles = [{
+    id: 1,
+    name: "Mark",
+    title: "Front-End developer",
+    bio: "I like to work with different front-end technologies and play video games."
+  }]
   return (
-   <>
-   <Greeting isLoggedIn={loggedIN}/>
-   <UserCheck/>
-   <ListRendering/>
-   <FruitList/>
-   </>
+    <>
+      <Greeting isLoggedIn={loggedIN} />
+      <UserCheck />
+      <ListRendering />
+      <FruitList />
+      {profiles.map((profiles) => {
+        return <Card    
+          key={profiles.id}
+          name={profiles.name}
+          title={profiles.title}
+          bio={profiles.bio}
+        />
+      })}
+
+    </>
   )
 }
 
---
+++ src/components/day-3-Props/scrip2.jsx
@@ -0,0 +1,9 @@
+export function MoodBoardItem ({color , image , description }){
+
+    return (
+        <div className="mood-board-item ">
+
+        </div>
+    )
+
+}

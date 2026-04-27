f481dc9d3e0c09970030bf26ed44f59bed0266aa
Ahmad-khan-1 <qurayshi246@gmail.com>
2026-04-27 09:43:10 +0500

    New Shopping App project

--- superheo-application-form/src/App.jsx
+++ superheo-application-form/src/App.jsx
@@ -1,10 +1,12 @@
 import ApplicationForm from "./components/Form";
+import ShoppingList from "./Shopping-app/ShoppingList";
 
 function App() {
   return (
     <>
       <h1>Superhero Application Form</h1>
       <ApplicationForm />
+      <ShoppingList />
     </>
   );
 }
